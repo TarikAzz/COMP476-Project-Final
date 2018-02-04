f533945dd65b1fa648244fdb3d2690abee1dc89f
majetteleger <ma_jetteleger@hotmail.com>
2018-02-04 16:05:51 -0500

    Working networked prototype

---
+++ Comp476Project/Assets/Scripts/Bullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private float _damage;
+
+    public void Initialize(float damage, float velocity)
+    {
+        _damage = damage;
+        GetComponent<Rigidbody>().velocity = transform.forward * velocity;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        var character = other.gameObject.GetComponent<Character>();
+        if (character != null)
+        {
+            character.TakeDamage(_damage);
+        }
+
+        Destroy(gameObject);
+    }
+}
