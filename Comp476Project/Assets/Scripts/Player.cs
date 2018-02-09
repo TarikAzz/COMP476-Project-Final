130c11969a928d53a2e24c7f187f573b9fb1d3a7
majetteleger <ma_jetteleger@hotmail.com>
2018-02-08 19:00:34 -0500

    Smooth breaking when reaching target

--- Comp476Project/Assets/Scripts/Player.cs
+++ Comp476Project/Assets/Scripts/Player.cs
@@ -15,6 +15,8 @@ public class Player : Character
 
     [Header("Movement")]
     public GameObject TargetIndicatorPrefab;
+    public Renderer Level;
+    public float MinVelocity;
     public float MaxVelocity;
     public float MaxAcceleration;
 
@@ -24,12 +26,25 @@ public class Player : Character
     private Vector3 _targetPosition;
     private Vector3 _velocity;
     private Vector3 _acceleration;
+    private float _topBounds;
+    private float _rightBounds;
+    private float _bottomBounds;
+    private float _leftBounds;
 
     void Start()
     {
         if (isLocalPlayer)
         {
             _spawnPoints = FindObjectsOfType<NetworkStartPosition>();
+
+            var moveAreaX = Level.bounds.size.x / 2;
+            var moveAreaZ = Level.bounds.size.z / 2;
+            var center = Level.bounds.center;
+
+            _topBounds = center.z + moveAreaZ;
+            _rightBounds = center.x + moveAreaX;
+            _bottomBounds = center.z - moveAreaZ;
+            _leftBounds = center.x - moveAreaX;
         }
     }
 
@@ -60,22 +75,24 @@ public class Player : Character
             _targetPosition = Target.transform.position;
             _targetPosition.y = transform.position.y;
 
-            _acceleration = Vector3.Normalize(_targetPosition - transform.position) * MaxAcceleration;
-
-            var potentialVelocity = _velocity + (Time.deltaTime * _acceleration);
-            _velocity = potentialVelocity.magnitude <= MaxVelocity ? potentialVelocity : MaxVelocity * Vector3.Normalize(potentialVelocity);
-
-            transform.position = transform.position + _velocity * Time.deltaTime;
-            transform.rotation = Quaternion.LookRotation(_velocity, Vector3.up);
+            PursueTarget();
         }
-    }
+        else if(_velocity.magnitude > MinVelocity)
+        {
+            PursueTarget();
+        }
 
+        RestrictPosition();
+    }
+    
     void OnTriggerEnter(Collider otherCollider)
     {
         if(otherCollider.gameObject == Target.gameObject)
         {
             Destroy(Target.gameObject);
             Target = null;
+
+            _targetPosition = transform.position - transform.forward * 5f;
         }
     }
 
@@ -113,6 +130,37 @@ public class Player : Character
             }
             
             transform.position = spawnPoint;
+        }
+    }
+
+    private void PursueTarget()
+    {
+        _acceleration = Vector3.Normalize(_targetPosition - transform.position) * MaxAcceleration;
+
+        var potentialVelocity = _velocity + (Time.deltaTime * _acceleration);
+        _velocity = potentialVelocity.magnitude <= MaxVelocity ? potentialVelocity : MaxVelocity * Vector3.Normalize(potentialVelocity);
+
+        transform.position = transform.position + _velocity * Time.deltaTime;
+        transform.rotation = Quaternion.LookRotation(_velocity, Vector3.up);
+    }
+
+    private void RestrictPosition()
+    {
+        if (transform.position.z > _topBounds)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, _topBounds);
+        }
+        if (transform.position.x > _rightBounds)
+        {
+            transform.position = new Vector3(_rightBounds, transform.position.y, transform.position.z);
+        }
+        if (transform.position.z < _bottomBounds)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, _bottomBounds);
+        }
+        if (transform.position.x < _leftBounds)
+        {
+            transform.position = new Vector3(_leftBounds, transform.position.y, transform.position.z);
         }
     }
 }
