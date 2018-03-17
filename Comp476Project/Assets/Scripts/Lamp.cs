81083a2f26f0b216a06670df87737be35d044b30
TarikAzz <tarikazzouni@hotmail.com>
2018-03-17 17:00:22 -0400

    Added Lamp
    Main.unity:
    --Added the lamp to the scene.
    
    Lamps.cs:
    --Gets the radius of the light, and makes the character spotted when within light range.
    
    Character.cs:
    --Checks all lamps to see if detected (this should be changed to collision detection instead..... CMON JONATHAN!!!).

---
+++ Comp476Project/Assets/Scripts/Lamp.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// JONATHAN'S PART
+public class Lamp : MonoBehaviour
+{
+    public GameObject point_light;
+    public float range;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        point_light = GameObject.Find("Head/Point Light");
+        range = point_light.GetComponent<Light>().range;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
