7135a73db5a28915d1305863c028c443b417231b
jonathan4210 <jonathan4210@gmail.com>
2018-03-18 19:35:03 -0400

    - Made Canvas prefab (same thing as original Canvas but with my layout of buttons)
    - Modified InGamePanel.cs to add toggle feature of new buttons
    
    - Added UnitSpawner.cs to interact with this toggle (not implemented yet)

--- Comp476Project/Assets/Scripts/InGamePanel.cs
+++ Comp476Project/Assets/Scripts/InGamePanel.cs
@@ -6,4 +6,74 @@ using UnityEngine.UI;
 public class InGamePanel : MonoBehaviour
 {
     public Text PlayerKind;
+
+    public GameObject controlContainer;
+    public List<Button> unitControls;
+    public ColorBlock buttonColors;
+
+    public bool controlLocked;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        controlLocked = false;
+
+        controlContainer = GameObject.Find("Unit Commands");
+
+        for(int i = 1; i <= controlContainer.transform.childCount; i++)
+        {
+            unitControls.Add(GameObject.Find("Unit " + i).GetComponent<Button>());
+        }
+
+
+
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Disable all buttons until you place the chosen unit
+        if (controlLocked == true)
+        {
+            for (int i = 0; i < unitControls.Count; i++)
+            {
+                unitControls[i].interactable = false;
+            }
+        }
+        else
+        {
+            for (int i = 0; i < unitControls.Count; i++)
+            {
+                
+                unitControls[i].interactable = true;
+
+                // Also, reset their colors back to white
+                buttonColors = unitControls[i].colors;
+                buttonColors.normalColor = Color.white;
+                buttonColors.disabledColor = Color.white;
+                unitControls[i].colors = buttonColors;
+            }
+        }
+    }
+
+
+
+
+    public void selectUnit(int ID)
+    {
+        buttonColors = unitControls[ID - 1].colors;
+        buttonColors.normalColor = Color.green;
+        buttonColors.disabledColor = Color.green;
+        unitControls[ID - 1].colors = buttonColors;
+
+        controlLocked = true;
+
+        
+    }
+
+
+
 }
---
+++ Comp476Project/Assets/Scripts/UnitSpawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitSpawner : MonoBehaviour
+{
+    public InGamePanel unitSelector;
+
+	// Use this for initialization
+	void Start ()
+    {
+        unitSelector = GameObject.Find("InGamePanel").GetComponent<InGamePanel>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
