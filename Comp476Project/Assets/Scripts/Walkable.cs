7963b12f371f1ca3d37d1bab62562ebbaa16a4d4
majetteleger <ma_jetteleger@hotmail.com>
2018-03-20 10:07:02 -0400

    Comments

--- Comp476Project/Assets/Scripts/Walkable.cs
+++ Comp476Project/Assets/Scripts/Walkable.cs
@@ -2,10 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// A walkable object
+/// </summary>
 public class Walkable : MonoBehaviour
 {
+    #region Constants
+
+    /// <summary>
+    /// The threshold at which a slope is considered a wall
+    /// </summary>
     private const float AngularThreshold = 45f;
 
+    #endregion
+
+    /// <summary>
+    /// Identifies the surface as a wall or not. Walls are not walkable
+    /// </summary>
+    /// <param name="normal">The surface normal</param>
+    /// <returns>Whether the surface is considered a wall or not</returns>
     public bool IsWall(Vector3 normal)
     {
         var angle = Vector2.Angle(
