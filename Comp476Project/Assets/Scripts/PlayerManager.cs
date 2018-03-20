2e98b402876873e8326305805ea11855b272895a
majetteleger <ma_jetteleger@hotmail.com>
2018-03-20 10:07:02 -0400

    Comments

--- Comp476Project/Assets/Scripts/PlayerManager.cs
+++ Comp476Project/Assets/Scripts/PlayerManager.cs
@@ -4,49 +4,60 @@ using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
 
+/// <summary>
+/// The object responsible for managing the player characters
+/// </summary>
 public class PlayerManager : NetworkBehaviour
 {
+    /// <summary>
+    /// The different roles a player can play
+    /// </summary>
     public enum PlayerKind
     {
         Infiltrator,
         Defender
     }
 
+    /// <summary>
+    /// The sensitivity of the camera controls
+    /// </summary>
     public float MouseSensitivity;
+
+    /// <summary>
+    /// The characters owned by the manager
+    /// </summary>
     public Character[] Characters;
 
+    /// <summary>
+    /// The player's role
+    /// </summary>
     public PlayerKind Kind { get; set; }
-
-    private int _id;
+    
+    /// <summary>
+    /// The player's HUD
+    /// </summary>
     private InGamePanel _inGamePanel;
 
+    /// <summary>
+    /// Intializes most of the variables when the player connects to the network
+    /// </summary>
     public override void OnStartLocalPlayer()
     {
         for (var i = 0; i < Characters.Length; i++)
         {
-            _id = NetworkManager.singleton.numPlayers;
-            Characters[i].OwnerId = _id;
             Characters[i].Owner = this;
             Characters[i].Colorize(Color.blue);
         }
 
         Kind = NetworkManager.singleton.numPlayers == 1 ? PlayerKind.Defender : PlayerKind.Infiltrator;
-
-        if (Kind == PlayerKind.Infiltrator)
-        {
-            /*var newCameraPosition = Camera.main.transform.position;
-            newCameraPosition.z = -newCameraPosition.z;
-
-            var newCameraRotation = Quaternion.Euler(55f, 180f, 0f);
-
-            Camera.main.transform.position = newCameraPosition;
-            Camera.main.transform.rotation = newCameraRotation;*/
-        }
-
+        
         _inGamePanel = FindObjectOfType<InGamePanel>();
         _inGamePanel.PlayerKind.text = Kind.ToString();
     }
 
+    /// <summary>
+    /// Updates character selection and camera movement
+    /// </summary>
     void Update()
     {
         if (!isLocalPlayer)
@@ -89,6 +100,11 @@ public class PlayerManager : NetworkBehaviour
         }
     }
 
+    /// <summary>
+    /// Whether the manager owns a specific character
+    /// </summary>
+    /// <param name="character">The character</param>
+    /// <returns>The manager's ownership of the character</returns>
     public bool OwnsCharacter(Character character)
     {
         return character.Owner != null && character.Owner.isLocalPlayer;
