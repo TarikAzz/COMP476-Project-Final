4f984ccc27e5c8f625cba92db8b7f2bc7e0c552b
jonathan4210 <jonathan4210@gmail.com>
2018-03-21 12:46:13 -0400

    - Created trap prefabs (with a trap script) for both infiltrator and defender.
    - Added both as spawnable objects in the Network Manager prefab.

---
+++ Comp476Project/Assets/Scripts/Trap.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    void OnCollisionEnter(Collision col)
+    {
+        // TEMP
+        if(col.gameObject.tag == "Infiltrator")
+        {
+            // ...
+        }
+        else if (col.gameObject.tag == "Defender")
+        {
+            // ...
+        }
+
+    }
+}
