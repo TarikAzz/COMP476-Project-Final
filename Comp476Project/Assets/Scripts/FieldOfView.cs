cdd4e9ff71d1e85f6f3a257ef7346d53edd49567
Tim Gottschalk <tim.gotts@gmail.com>
2018-03-21 12:43:02 -0400

    Field of viiew initial implementation + editor script

---
+++ Comp476Project/Assets/Scripts/FieldOfView.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Boo.Lang.Runtime.DynamicDispatching;
+using UnityEngine;
+
+public class FieldOfView : MonoBehaviour
+{
+
+    public float ViewRadius;
+    [Range(0,360)]
+    public float ViewAngle;
+
+    public LayerMask targetMask;
+    public LayerMask obstacleMask;
+
+    [HideInInspector]
+    public List<Transform> visibleTargets = new List<Transform>();
+
+
+    public float meshResolution;
+
+    void Start()
+    {
+        StartCoroutine("FindTargetsWithDelay",.2f);
+    }
+
+
+    IEnumerator FindTargetsWithDelay(float delay)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(delay);
+            FindVisibleTargets();
+        }
+    }
+
+    void FindVisibleTargets()
+    {
+        visibleTargets.Clear();
+        Collider[] targetsInViewRadius = Physics.OverlapSphere(transform.position, ViewRadius, targetMask);
+        for (int i = 0; i < targetsInViewRadius.Length; i++)
+        {
+            Transform target = targetsInViewRadius[i].transform;
+            Vector3 directionToTarget = (target.position - transform.position).normalized;
+            if (Vector3.Angle(transform.forward, directionToTarget) < ViewAngle / 2)
+            {
+                float distanceToTarget = Vector3.Distance(transform.position, target.position);
+                if (!Physics.Raycast(transform.position,directionToTarget,distanceToTarget,obstacleMask))
+                {
+                    visibleTargets.Add(target);
+                }
+            }
+        }
+
+    }
+
+    public Vector3 DirectionFromAngle(float angleInDegrees, bool angleIsGlobal)
+    {
+        if (!angleIsGlobal)
+        {
+            angleInDegrees += transform.eulerAngles.y;
+        }
+        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+    }
+
+
+}
