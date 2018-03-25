889f779aade5f7ef0067eb54f4fdda926a28a5f1
jonathan4210 <jonathan4210@gmail.com>
2018-03-25 18:14:18 -0400

    - Infiltrator traps can now stun defender units for a brief time.
    - Stun status has its own particle effect.

---
+++ Comp476Project/Assets/Scripts/DestroyParticles.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyParticles : MonoBehaviour
+{
+    private ParticleSystem particles;
+
+    // Use this for initialization
+    void Start()
+    {
+        particles = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (particles.isPlaying)
+            return;
+
+        Destroy(gameObject);
+    }
+}
--- Comp476Project/Assets/Scripts/Trap.cs
+++ Comp476Project/Assets/Scripts/Trap.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class Trap : MonoBehaviour
+public class Trap : NetworkBehaviour
 {
+    // Defined in Inspector
+    public GameObject StunParticles;
 
     // Use this for initialization
     void Start()
@@ -20,15 +23,13 @@ public class Trap : MonoBehaviour
 
     void OnCollisionEnter(Collision col)
     {
-        if(/*col.gameObject.tag == "Infiltrator" &&*/ tag == "iTrap")
+        if(col.gameObject.tag == "Good" && tag == "iTrap")
         {
-            
+            col.gameObject.GetComponent<Character>().IsStunned = true;
+            col.gameObject.GetComponent<Character>()._navMeshAgent.SetDestination(transform.position);
+
+            GameObject effect = Instantiate(StunParticles, col.gameObject.transform.position, Quaternion.identity);
+            NetworkServer.Spawn(effect);
         }
-        /*
-        else if (col.gameObject.tag == "Defender")
-        {
-            
-        }
-        */
     }
 }
