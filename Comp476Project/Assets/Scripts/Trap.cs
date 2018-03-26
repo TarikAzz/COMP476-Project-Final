98b7413821666c32bfacc4a9cfb6f5bcc1529947
jonathan4210 <jonathan4210@gmail.com>
2018-03-25 20:10:56 -0400

    - Defender traps now reveal infiltrators within a certain range.
    - Both traps are now functional.

--- Comp476Project/Assets/Scripts/Trap.cs
+++ Comp476Project/Assets/Scripts/Trap.cs
@@ -8,16 +8,33 @@ public class Trap : NetworkBehaviour
     // Defined in Inspector
     public GameObject StunParticles;
 
+    // Keep track of all Infiltrators for Defender traps
+    public GameObject[] Infiltrators;
+
+
     // Use this for initialization
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Only run this if it's a Defender Trap
+        if (tag == "Trap")
+        {
+            Infiltrators = GameObject.FindGameObjectsWithTag("Bad");
 
+            // Scan all infiltrators to see if any are within range to be spotted
+            for (int i = 0; i < Infiltrators.Length; i++)
+            {
+                if (Vector3.Distance(transform.position, Infiltrators[i].transform.position) <= 8f)
+                {
+                    Infiltrators[i].GetComponent<Character>().IsSpotted = true;
+                }
+            }
+        }
     }
 
 
