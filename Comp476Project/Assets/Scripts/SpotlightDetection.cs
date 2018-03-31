30e9cc45564baf7fd4e33dd61b81bed2dc2469e7
jonathan4210 <jonathan4210@gmail.com>
2018-03-31 14:12:53 -0400

    - Fixed lamp and camera functionality. They now properly reveal infiltrators.
    - Modified camera prefab's look

---
+++ Comp476Project/Assets/Scripts/SpotlightDetection.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpotlightDetection : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    // Reveal infiltrators when entering the spotlight
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Bad")
+        {
+            col.gameObject.GetComponent<Character>().IsSpotted = true;
+        }
+    }
+
+    // Hide infiltrators when leaving the spotlight
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == "Bad")
+        {
+            col.gameObject.GetComponent<Character>().IsSpotted = false;
+        }
+    }
+}
