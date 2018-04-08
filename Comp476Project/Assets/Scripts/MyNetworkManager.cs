d0dafedbf0139f5283f08143451583005314da3c
jonathan4210 <jonathan4210@gmail.com>
2018-04-08 15:49:03 -0400

    - Created title screen to replace default NetworkHUD (accesses NetworkManager).    - Some functionalities still use the default Network HUD.

--- Comp476Project/Assets/Scripts/MyNetworkManager.cs
+++ Comp476Project/Assets/Scripts/MyNetworkManager.cs
@@ -2,22 +2,109 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class MyNetworkManager : NetworkManager
 {
+    // Defined in Inspector
+    public Canvas TitleScreen;
+    public Text IP_Address;
+
+    // Use this for initialization
+    void Start()
+    {
+        ToggleNetworkHUD(false);
+        
+    }
+
+
+
+
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+
+
+    public void ToggleNetworkHUD(bool state)
+    {
+        gameObject.GetComponent<NetworkManagerHUD>().showGUI = state;
+    }
+
+
+
+
+    
+
+
+    public void CreateSession()
+    {
+        TitleScreen.enabled = false;
+        ToggleNetworkHUD(true);
+        StartHost();
+    }
+
+
+
+    public void JoinSession()
+    {
+        if (IP_Address.text == "")
+        {
+            networkAddress = "localhost";
+        }
+        else
+        {
+            networkAddress = IP_Address.text;
+        }
+
+        TitleScreen.enabled = false;
+        ToggleNetworkHUD(true);
+        StartClient();
+        
+    }
+
+
+
+
+    public void MatchMaker()
+    {
+        TitleScreen.enabled = false;
+        ToggleNetworkHUD(true);
+        StartMatchMaker();
+    }
+
+
+
+
+
+
     public override void OnStopServer()
     {
+        TitleScreen.enabled = true;
         HandleDisconnection();
-
         base.OnStopServer();
     }
 
     public override void OnStopClient()
     {
+        TitleScreen.enabled = true;
         HandleDisconnection();
-
         base.OnStopClient();
     }
+
+
+
+    
+
+
+
 
     private void HandleDisconnection()
     {
