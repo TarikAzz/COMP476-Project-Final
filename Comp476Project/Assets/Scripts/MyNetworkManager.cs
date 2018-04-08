4fbc20e4ace8f48c9ecf1e283646ed1c5003e08d
jonathan4210 <jonathan4210@gmail.com>
2018-04-08 18:29:18 -0400

    - Bug fixes for the title screen.

--- Comp476Project/Assets/Scripts/MyNetworkManager.cs
+++ Comp476Project/Assets/Scripts/MyNetworkManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.Networking.Match;
 using UnityEngine.UI;
 
 public class MyNetworkManager : NetworkManager
@@ -10,39 +11,29 @@ public class MyNetworkManager : NetworkManager
     public Canvas TitleScreen;
     public Text IP_Address;
 
+    // Used to detect when matchmaker is ON
+    public bool Matchmaking;
+
     // Use this for initialization
     void Start()
     {
+        // Hide HUD at the start, title screen handles networking options
         ToggleNetworkHUD(false);
     }
-
-
-
-
-
-
-
-
+    
     // Update is called once per frame
     void Update()
     {
-
+        // A hack to "override" the Disable Match Maker button
+        if(Matchmaking && matchMaker == null)
+        {
+            Matchmaking = false;
+            TitleScreen.enabled = true;
+            ToggleNetworkHUD(false);
+        }
     }
 
-
-
-
-    public void ToggleNetworkHUD(bool state)
-    {
-        gameObject.GetComponent<NetworkManagerHUD>().showGUI = state;
-    }
-
-
-
-
-    
-
-
+    // Listener for the Create Session button
     public void CreateSession()
     {
         TitleScreen.enabled = false;
@@ -50,10 +41,10 @@ public class MyNetworkManager : NetworkManager
         StartHost();
     }
 
-
-
+    // Listener for the Join Session button
     public void JoinSession()
     {
+        // Have the default address set to localhost
         if (IP_Address.text == "")
         {
             networkAddress = "localhost";
@@ -66,45 +57,36 @@ public class MyNetworkManager : NetworkManager
         TitleScreen.enabled = false;
         ToggleNetworkHUD(true);
         StartClient();
-        
     }
 
-
-
-
+    // Listener for the Match Maker button
     public void MatchMaker()
     {
         TitleScreen.enabled = false;
         ToggleNetworkHUD(true);
+        Matchmaking = true;
         StartMatchMaker();
     }
 
-
-
-
-
-
+    // Override to bring back the title screen and to disable the default HUD again
     public override void OnStopServer()
     {
         TitleScreen.enabled = true;
+        ToggleNetworkHUD(false);
         HandleDisconnection();
         base.OnStopServer();
     }
 
+    // Override to bring back the title screen and to disable the default HUD again
     public override void OnStopClient()
     {
         TitleScreen.enabled = true;
+        ToggleNetworkHUD(false);
         HandleDisconnection();
         base.OnStopClient();
     }
 
-
-
-    
-
-
-
-
+    // Handles disconnection
     private void HandleDisconnection()
     {
         Camera.main.transform.position = new Vector3(0f, 84f, 65f);
@@ -120,5 +102,11 @@ public class MyNetworkManager : NetworkManager
             mainManager.DefenderReady = false;
             mainManager.InfiltratorReady = false;
         }
+    }
+
+    // Toggle the default HUD appearence
+    public void ToggleNetworkHUD(bool state)
+    {
+        gameObject.GetComponent<NetworkManagerHUD>().showGUI = state;
     }
 }
